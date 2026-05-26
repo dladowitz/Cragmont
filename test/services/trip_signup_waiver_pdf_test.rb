342695483c3303b4f58dd282d30e399b88f7ffd1
David Ladowitz <david@ladowitz.com>
2026-05-26 11:36:45 -0700

    Polish waiver signing flow

--- test/services/trip_signup_waiver_pdf_test.rb
+++ test/services/trip_signup_waiver_pdf_test.rb
@@ -12,4 +12,14 @@ class TripSignupWaiverPdfTest < ActiveSupport::TestCase
     assert_operator pdf.bytesize, :>, 1_000
     assert_match(/\/Count [2-9]\b/, pdf)
   end
+
+  test "formats metadata times in twelve hour Pacific time" do
+    signup = TripSignup.create!(trip: trips(:yosemite), user: users(:sam))
+    signature = WaiverSignatureData.new(SIGNATURE_DATA_URL)
+    service = TripSignupWaiverPdf.new(trip_signup: signup, signature_png: signature.bytes)
+
+    formatted_time = service.send(:formatted_metadata_time, Time.utc(2026, 5, 27, 1, 4))
+
+    assert_equal "May 26, 2026 6:04 PM PDT", formatted_time
+  end
 end
