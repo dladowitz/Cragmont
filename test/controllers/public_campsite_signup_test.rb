412dd5ee25494896248326d74ce6b8b4f7225cd4
David Ladowitz <david@ladowitz.com>
2026-06-16 21:29:04 -0700

    Adding What to Expect page

--- test/controllers/public_campsite_signup_test.rb
+++ test/controllers/public_campsite_signup_test.rb
@@ -39,7 +39,17 @@ class PublicCampsiteSignupTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select "h1", "What to expect on a Cragmont trip"
-    assert_select "p", text: /Cragmont trips are community camping and climbing weekends/
+    assert_select ".trip-expectation-intro strong", "Cragmont trips are a shared base camp, not a guided trip."
+    assert_select "h2", "Do I need a partner to come on a trip?"
+    assert_select "h2", "Will Cragmont teach me to climb outside?"
+    assert_select "h2", "Do you know any professional climbing companies I can take classes from?"
+    assert_select "a[href='https://chat.whatsapp.com/DngyoeuFkg61oCZXPyyAgX?mode=gi_t']", text: "WhatsApp Community"
+    assert_select "a[href='https://www.cragmontclimbingclub.org/join-the-list']", text: "sign up for our email list"
+    assert_select "a[href='https://www.verticalpursuitsclimbing.com/']", text: "Vertical Pursuits"
+    assert_select "a[href='https://loversleap.net/']", text: "Lovers Leap Guides"
+    assert_select "a[href='https://www.saanoadventures.com/']", text: "SANNO Adventures"
+    assert_select "a[href='#{new_help_request_path}']", text: "Get Help"
+    assert_select ".background-image-caption", "Fairview Dome, Yosemite National Park"
   end
 
   test "trips index shows archived trips five at a time" do
