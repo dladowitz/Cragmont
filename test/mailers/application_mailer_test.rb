63f22b6e5e61608e9e12a7e7ef667f3928ff04a9
David Ladowitz <david@ladowitz.com>
2026-06-17 14:32:16 -0700

    fix test

--- test/mailers/application_mailer_test.rb
+++ test/mailers/application_mailer_test.rb
@@ -17,7 +17,7 @@ class ApplicationMailerTest < ActionMailer::TestCase
     assert_includes html, "Climbing Club"
     assert_includes html, "font-family: Arial, Helvetica, sans-serif"
     assert_includes html, "width: 620px"
-    assert_includes html, "Cragmont is not a teaching organization. It's a base camp."
+    assert_includes html, "Cragmont is not a teaching organization. It's a social base camp."
     assert_includes html, "email-liability-note"
     assert_not_includes html, "vent-five-emperor-boulder"
     assert_not_includes html, "Marin Coast, Emperor Boulder"
